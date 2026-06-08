8bde610cd89932244b523c9ea61b7c358f0e0303
giusscos <giusscos@icloud.com>
2026-06-08 10:57:40 +0200

    Initial Commit

---
+++ pola/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  pola
+//
+//  Created by Giuseppe Cosenza on 08/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
